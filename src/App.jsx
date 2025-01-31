08667cb4591b3c7b96d5da2aa2a8694aaceb47ca
rishu123456 <rishu25112001@gamil.com>
2025-01-31 18:07:44 +0530

    update couner app1

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,15 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
- 
+  function updateCount(){
+    setCount(count+1);
+  }
 
   return (
     <>
-    <CustomButton count={count} setCount={setCount}></CustomButton>
-      
+      <button onClick={updateCount}>count is {count}</button>
     </>
   )
-
-  function CustomButton(props){
-    function UpdateCount(){
-     props.setCount(count+1);
-    }
-
-    return <button onClick={UpdateCount}>count is {count}</button>
-  }
 }
 
 export default App
